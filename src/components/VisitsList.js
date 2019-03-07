f4c84219b96f165ff9c1bae1e44a7d6d376d5733
TheScobey <TheScobey@users.noreply.github.com>
2019-03-07 21:53:53 +1100

    Fix loading animation

--- src/components/VisitsList.js
+++ src/components/VisitsList.js
@@ -127,7 +127,7 @@ class VisitsList extends Component {
           <Typography variant="h5" component="h3">
             {this.props.auth.displayName}'s Burgers Consumed
         </Typography>
-          <div style={{display: 'flex'}}>
+          <div style={{ display: 'flex' }}>
             {
               DateChoices.map((choice, i) =>
                 <Button
@@ -137,7 +137,7 @@ class VisitsList extends Component {
                   color="primary"
                   className={this.props.classes.margin}
                   onClick={this.onDateEndChange.bind(this, DateChoices[i])}>
-                    {choice.label}
+                  {choice.label}
                 </Button>
               )
             }
